b4c0f6bc62a397101e3b408055bbe48b2185997c
Bartosz Gembala <bartoszgembala95@gmail.com>
2022-09-30 14:17:29 +0200

    Added component comunication with Input and Output

--- src/app/components/recipes/recipe-item/recipe-item.component.ts
+++ src/app/components/recipes/recipe-item/recipe-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -8,6 +8,12 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeItemComponent implements OnInit {
   @Input() recipe: Recipe;
+  @Output() emitRecipeData = new EventEmitter<Recipe>();
+
+  onClick() {
+    this.emitRecipeData.emit(this.recipe);
+  }
+
   constructor() {}
 
   ngOnInit(): void {}
